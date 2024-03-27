8ecb51617bfcec42678ce36e92f3e6a69db4313a
Diego Castillo <totitodjc@gmail.com>
2024-03-27 18:35:42 -0400

    a6 part 3

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import UserRoutes from './Users/routes.js';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import cors from "cors";
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
