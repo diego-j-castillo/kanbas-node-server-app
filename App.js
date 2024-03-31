27f606419da6dcb130f2efa9518b0e0ebc64ac79
Diego Castillo <totitodjc@gmail.com>
2024-03-31 13:11:21 -0400

    a6 complete part 2

--- App.js
+++ App.js
@@ -10,13 +10,14 @@ import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import SessionRoutes from "./SessionRoutes.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
   })
 );
 const sessionOptions = {
