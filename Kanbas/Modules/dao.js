8294ccadb389ed59c081ed50368c613c9b23615c
Diego Castillo <totitodjc@gmail.com>
2024-04-05 19:37:43 -0400

    fix module and course

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -1,7 +1,7 @@
 import model from "./model.js";
 export const createModule = (module) => {
   delete module._id;
-  model.create(module);
+  return model.create(module);
 };
 export const updateModule = (moduleId, module) =>  model.updateOne({ _id: moduleId }, { $set: module });
 export const deleteModule = (moduleId) => model.deleteOne({ _id: moduleId });
